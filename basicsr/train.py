57bf6705193f1a149a20a9422ba53ba60721f792
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-25 21:45:40 +0900

    Update train.py

--- basicsr/train.py
+++ basicsr/train.py
@@ -22,14 +22,10 @@ import numpy as np
 
 def parse_options(is_train=True):
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-opt', type=str, required=True, help='Path to option YAML file.')
-    parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm'],
-        default='none',
-        help='job launcher')
+    parser.add_argument('-opt', type=str, required=True, help='Path to option YAML file.')
+    parser.add_argument('--launcher', choices=['none', 'pytorch', 'slurm'], default='none', help='job launcher')
     parser.add_argument('--local_rank', type=int, default=0)
+  
     args = parser.parse_args()
     opt = parse(args.opt, is_train=is_train)
 
